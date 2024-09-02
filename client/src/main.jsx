3645d72ce803a888564a02f7b6b930455bf4ff21
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-03 02:52:40 +0400

    Add redux persist to application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { store,persistor } from "./redux/store.js";
+import {PersistGate} from 'redux-persist/integration/react'
 
 createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
     <App />
   </Provider>
+  </PersistGate>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./userSlice/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+// 1- Create a root reducer:
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// 2- configure store
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  // MiddleWare to prevent default error in redux:
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
