ae261072d78e425d8a95fd34634e4120295f617f
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-02 19:30:41 +0400

    create footer components

--- client/src/components/FooterCom.jsx
+++ client/src/components/FooterCom.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
 
 const FooterCom = () => {
   return (
-    <div>FooterCom</div>
-  )
-}
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-blue-500 via-cyan-500 to-green-500 rounded-lg text-white">
+                Free Pen
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" className="cursor-pointer"/>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Free Pen Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" className="cursor-pointer"/>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/mohammadalnseirat"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" className="cursor-pointer"/>
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Mohammad Alnseirat"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon
+              href="#"
+              icon={BsFacebook}
+              className="text-[#1877f2]"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+              className="text-[#fd1d1d]"
+            />
+            <Footer.Icon href="#" icon={BsTwitter} className="text-[#1da1f2]" />
+            <Footer.Icon
+              href="https://github.com/mohammadalnseirat"
+              icon={BsGithub}
+              className="text-[#333] dark:text-gray-100"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsDribbble}
+              className="text-[#ff8833]"
+            />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+};
 
-export default FooterCom
+export default FooterCom;
