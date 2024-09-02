4ef2bed73989e6f384a67c13d3e7a0587fd6fc15
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-03 00:21:04 +0400

    create Sign In Api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { handleErrors } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
+// 1- Function to Create Sign Up Api Route:
 export const signUpPost = async (req, res, next) => {
   // Implement signup logic here
   const { username, email, password } = req.body;
@@ -46,6 +47,47 @@ export const signUpPost = async (req, res, next) => {
     res.status(201).json({ message: "User Created Successfully!." });
   } catch (error) {
     console.log("Error in Creating Sign Up Api route", error.message);
+    next(error);
+  }
+};
+
+// 2- Function to create sign in api routes:
+export const signInPost = async (req, res, next) => {
+  // Implement Sign In Logic Here:
+  const { email, password } = req.body;
+  if(!email || !password || email === '' || password === ' '){
+    return next(handleErrors(403, "Please Fill All Required Fields!."));
+  }
+  try {
+    // Find the User Account:
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(handleErrors(403, "Invalid Email or Password!."));
+    }
+
+    // compare Password:
+    const passwordMatched = bcryptjs.compareSync(password, user.password);
+    if (!passwordMatched) {
+      return next(handleErrors(403, "Invalid Password!."));
+    }
+
+    // Generate JWT:
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    // Hide the password:
+    const { passowrd, ...rest } = user._doc;
+
+    // save the token:
+    res
+      .cookie("access_token", token, {
+        httpOnly: true, // prevent XSS attacks, cross site scripting attack
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // // prevents CSRF attack, cross-site request forgery attack
+        maxAge: 2 * 24 * 60 * 60 * 100, // 2 Days
+      })
+      .status(201)
+      .json(rest);
+  } catch (error) {
+    console.log("Error in Sign In Api route", error.message);
     next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUpPost } from "../controllers/auth.controller.js";
+import { signInPost, signUpPost } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUpPost);
+router.post('/signin',signInPost)
 
 export default router;
