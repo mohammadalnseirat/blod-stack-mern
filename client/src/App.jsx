f9dde066812d6ff609a10b8c97f924e285fb02e9
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-03 20:06:41 +0400

    Make Dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Profile from './pages/Profile'
-import Dashboard from './pages/Dashboard'
-import Header from './components/Header'
-import FooterCom from './components/FooterCom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import FooterCom from "./components/FooterCom";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/sign-in' element={<SignIn/>}/>
-      <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-    </Routes>
-    <FooterCom/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+      <FooterCom />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
