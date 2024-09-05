98712a1dd491d19535fe06e365b100165be5ac6c
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-05 21:18:57 +0400

    create delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -81,3 +81,17 @@ export const updateUser_put = async (req, res, next) => {
     next(error);
   }
 };
+
+// 3- Function to delete a user from the database
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(handleErrors(403, "Your not allowed to delete this user!."));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User deleted successfully!");
+  } catch (error) {
+    console.log("Error deleting user ", error.message);
+    next(error);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { test_get, updateUser_put } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  test_get,
+  updateUser_put,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test_get);
 router.put("/update/:userId", verifyToken, updateUser_put); // Protects the routes this is bcz no one can update the user if it is not authenticated.
+router.delete("/delete/:userId", verifyToken, deleteUser); // Protects the routes this is bcz no
 
 export default router;
