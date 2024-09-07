d4eccca5e34daf87553faec1557450db0b501a2d
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-07 04:48:43 +0400

    Add posts section to sidebar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "flowbite-react";
-import { HiArrowCircleRight, HiUser } from "react-icons/hi";
+import { HiArrowCircleRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signOutSuccess } from "../redux/userSlice/userSlice";
-import {useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 
 const DashSidebar = () => {
   const location = useLocation();
   const [tab, setTab] = useState();
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -15,48 +16,61 @@ const DashSidebar = () => {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-
-  const handleDelete = async()=>{
+  const handleDelete = async () => {
     try {
       // create a resposne:
-      const res = await fetch('/api/user/signout',{
-        method: 'POST'
-      })
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
       // convert data to json object:
-      const data = await res.json()
-      if(!res.ok){
-        console.log(data.message)
-      }else{
-        dispatch(signOutSuccess())
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signOutSuccess());
       }
-      
     } catch (error) {
-      console.log("Error signing out user ", error.message)
+      console.log("Error signing out user ", error.message);
       // dispatch error action here for UI feedback:
       // dispatch(signOutFailure(error.message)) // Uncomment when using Redux slice for error handling.  The error message will be displayed in the UI.  The action would be dispatched to the Redux store to update the state.  The UI would then show an error message.  Note: This is a placeholder and should be replaced with actual error handling logic.  This example assumes the error message is returned in the response data.  In a real-world application, you would need to handle the error response differently.  For example, you might want to retry the request, display a different error message, or handle it differently based on the status code of the response.  For more information on error handling in Redux, see the official Redux documentation: https://redux.js.org/usage/advanced/async-actions#error-handling-in-
-      
     }
-  }
+  };
   return (
     <Sidebar className="w-full md:w-56 border-r border-t-2 border-r-gray-300 dark:border-t-transparent dark:border-r-transparent">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-2">
           <Link to={"/dashboard?tab=profile"}>
             <Sidebar.Item
-            as={'div'}
+              as={"div"}
               icon={HiUser}
-              label={"User"}
+              label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor={"dark"}
               active={tab === "profile"}
-              className="cursor-pointer"
-              
+              className="font-[600]"
             >
               Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item onClick={handleDelete} icon={HiArrowCircleRight} className="cursor-pointer">
+          {currentUser && currentUser.isAdmin && (
+            <Link to={"/dashboard?tab=posts"}>
+              <Sidebar.Item
+                as="div"
+                icon={HiDocumentText}
+                labelColor={"dark"}
+                active={tab === "posts"}
+                className="font-[600]"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Sidebar.Item
+            onClick={handleDelete}
+            icon={HiArrowCircleRight}
+            className="cursor-pointer font-[600]"
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
