14441266c33cffeafde491569cc49e204f574f91
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-12 07:09:42 +0400

    Add recent Articel to the post page ui

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdOutlineAccessTimeFilled } from "react-icons/md";
+
+const PostCard = ({ post }) => {
+  return (
+    <div className="group relative  w-full border-2 border-cyan-600  h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all duration-300">
+      <Link to={`/post/${post.slug}`}>
+        <img
+          src={post.image}
+          alt={post.title}
+          className="h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20"
+        />
+      </Link>
+      <div className="p-3 flex flex-col gap-2">
+        <p className="font-semibold line-clamp-2 text-xl dark:text-cyan-500">
+          {post.title}
+        </p>
+        <div className="flex items-center justify-between">
+          <span className="italic">{post.category}</span>
+          <span className="flex items-center gap-1 text-gray-500 dark:text-cyan-500 text-sm">
+            <MdOutlineAccessTimeFilled className="text-lg" />
+            {new Date(post.createdAt).toLocaleDateString()}
+          </span>
+        </div>
+        <Link
+          to={`/post/${post.slug}`}
+          className="z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2"
+        >
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,8 +1,10 @@
-import { Button, Spinner } from "flowbite-react";
+import { Alert, Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import { MdError } from "react-icons/md";
+import PostCard from "../components/PostCard";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -10,6 +12,8 @@ const PostPage = () => {
   const [error, setError] = useState(false);
   // state to save the data when fetching posts:
   const [post, setPost] = useState(null);
+  // state to save recent posts:
+  const [recentPosts, setRecentPosts] = useState(null);
   // useEffect to fetch the post based on the slug:
   useEffect(() => {
     const fetchPost = async () => {
@@ -39,6 +43,34 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  // useEffect to get recent posts:
+  useEffect(() => {
+    const fetchRecentPosts = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        // create response:
+        const res = await fetch(`/api/post/getposts?limit=4`);
+        // convert response to json:
+        const data = await res.json();
+        if (!res.ok) {
+          setError(data.message);
+          setLoading(false);
+          return;
+        }
+        if (res.ok) {
+          setRecentPosts(data.posts);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error.message);
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchRecentPosts();
+  }, []);
 
   //   Put the loading :
   if (loading) {
@@ -83,6 +115,20 @@ const PostPage = () => {
         <CallToAction />
       </div>
       <CommentSection postId={post._id} />
+      <div className="flex flex-col items-center justify-center mb-5">
+        <h1 className="text-2xl font-semibold  mt-5 text-gray-600 dark:text-cyan-400 font-mono">
+          Recent Article
+        </h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center items-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
+      {error && (
+        <Alert color={"failure"} icon={MdError} className="font-semibold mt-5">
+          {error}
+        </Alert>
+      )}
     </main>
   );
 };
