69c75b63d3463256c57b7b0842a394c0468be6cb
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-13 10:01:12 +0400

    add pre installation to t

--- api/index.js
+++ api/index.js
@@ -6,9 +6,13 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+// Serve static files from the build folder.:
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -17,6 +21,15 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+// Serve static files from the build folder.:
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// Handle SPA (Single Page Application) requests:
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectDB();
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,7 +4,9 @@ import CallToAction from "../components/CallToAction";
 const Projects = () => {
   return (
     <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
-      <h1 className="text-3xl text-gray-700 dark:text-cyan-500 font-mono font-semibold">Projects</h1>
+      <h1 className="text-3xl text-gray-700 dark:text-cyan-500 font-mono font-semibold">
+        Projects
+      </h1>
       <p className="text-md text-gray-500">
         Build fun and engaging projects while learning HTML, CSS, and JavaScript
         and Other Features in Java Script!
